7fceddc6ed9000dfa77d71f4b66539155d7db0b0
jbjalandoon <jerome.jalandoon@gmail.com>
2025-07-02 22:01:24 +0800

    add redis url

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,16 +1,7 @@
 import { createClient } from 'redis'
 
-// const REDIS_HOST = process.env.REDIS_HOST || 'redis'
-// const REDIS_PASSWORD = process.env.REDIS_PASSWORD
-// const REDIS_PORT = process.env.REDIS_PORT || (6379 as number)
-
-const REDIS_HOST = 'growing-garfish-40249.upstash.io'
-const REDIS_PORT = 6380
-const REDIS_PASSWORD =
-  'AZ05AAIjcDE2Nzk5ZDg0ZTcwOTc0Mjk2OWM2YWY5MzU0NzAxMDlhNnAxMA'
-
 const redis = createClient({
-  url: 'rediss://default:AZ05AAIjcDE2Nzk5ZDg0ZTcwOTc0Mjk2OWM2YWY5MzU0NzAxMDlhNnAxMA@growing-garfish-40249.upstash.io:6379',
+  url: process.env.REDIS_URL,
 })
 
 redis.on('connect', (con) => {
