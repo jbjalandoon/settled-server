72e19c93d10a61c1f3911e69cd5d3d08cbb98266
jbjalandoon <jerome.jalandoon@gmail.com>
2025-07-14 10:30:43 +0800

    update redis env

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -3,8 +3,7 @@ import { createClient } from 'redis'
 console.log(process.env.REDIS_PORT)
 const redis = createClient({
   url:
-    process.env.REDIS_URL ||
-    'rediss://default:AZ05AAIjcDE2Nzk5ZDg0ZTcwOTc0Mjk2OWM2YWY5MzU0NzAxMDlhNnAxMA@growing-garfish-40249.upstash.io:6379',
+    process.env.REDIS_URL
 })
 
 redis.on('connect', (con) => {
